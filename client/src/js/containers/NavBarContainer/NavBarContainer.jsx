cdbb38034364abaac7a60dd4155d559e8d36788c
Kennaka5 <Kennaka5@gmail.com>
2018-01-12 17:28:05 -0800

    Sign in Modal

--- client/src/js/containers/NavBarContainer/NavBarContainer.jsx
+++ client/src/js/containers/NavBarContainer/NavBarContainer.jsx
@@ -7,13 +7,59 @@ class NavBarContainer extends React.Component {
   }
 
 
+
   render() {
     return (
+      <div>
         <div className="navBar">
-        <ul>
-          <li ><a className="navLogo" href="#home">Door Stop Food</a></li>
-          <li ><a className="navLogIn" href="#about">Log in</a></li>
-        </ul>
+          <ul>
+            <li ><a className="navLogo" href="#home">Door Stop Food</a></li>
+            <li ><a className="navLogIn" data-toggle="modal" data-target="#exampleModalCenter">
+              Login</a>
+            </li>
+          </ul>
+        </div>
+        <div className="login">
+          <div className="modal fade" id="exampleModalCenter" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="exampleModalLongTitle">Sign-In</h5>
+                  <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <form>
+                    <div className="form-group">
+                      <label>Username</label>
+                      <input className="form-control" placeholder="Username" />
+                    </div>
+                    <div className="form-group">
+                      <label >Password</label>
+                      <input className="form-control" placeholder="Password" />
+                    </div>
+                    <div className="form-group">
+                    <button type="button" 
+                      className="btn btn-primary form-control" 
+                      data-dismiss="modal" 
+                      aria-label="Close">
+                      Login
+                    </button>
+                    </div>
+                    <div className="form-group pt-3">
+                    <button type="button" 
+                      className="btn btn-primary form-control" 
+                      href= {`#/signup/`}>
+                      Sign Up
+                    </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
