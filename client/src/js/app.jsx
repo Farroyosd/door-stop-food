1cd8654fb7aba938688b365faabfc546c74c0921
Francisco Arroyo <arroyo.franciscojr@gmail.com>
2018-01-12 17:38:58 -0800

    added SignUpContainer and updated rootstore

--- client/src/js/app.jsx
+++ client/src/js/app.jsx
@@ -7,6 +7,7 @@ import HomeContainer from './containers/HomeContainer';
 import SearchResultsContainer from './containers/SearchResultsContainer';
 import MenuContainer from './containers/MenuContainer';
 import CheckoutContainer from './containers/CheckoutContainer'
+import SignUpContainer from './containers/SignUpContainer'
 import '../css/style.less';
 import FooterContainer from './containers/FooterContainer';
 
@@ -21,6 +22,7 @@ export default class App extends Component {
         <Route exact path='/searchresults' component={ SearchResultsContainer } />
         <Route exact path='/menu' component={ MenuContainer } />
         <Route exact path='/checkout' component={ CheckoutContainer } />
+        <Route exact path='/signup' component={ SignUpContainer } />
       <FooterContainer/>
       </div>
     </Router>
