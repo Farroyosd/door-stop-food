5cf104dc66f6ec4d74563e5b7a376ff42103b52f
Kennaka5 <Kennaka5@gmail.com>
2018-01-14 20:00:35 -0800

    staticNav

---
+++ client/src/js/containers/NavBarContainer/navbarActions.js
@@ -0,0 +1,20 @@
+export function updateUserName(username) {
+    return {
+      type: 'USER_NAME',
+      payload: { username }
+    };
+  }
+  
+  export function updatePassword(password) {
+    return {
+      type: 'PASSWORD',
+      payload: { password }
+    };
+  }
+
+  export function loggedInUser(userLogin) {
+    return {
+    type: 'USER_LOGIN',
+    payload: { userLogin } 
+  };
+}
